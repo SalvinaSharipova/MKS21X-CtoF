21381f176e2b1fb345d9f39e41dfa89cd815409b
Salvina Sharipova <90580234+SalvinaSharipova@users.noreply.github.com>
2021-09-23 22:45:27 -0400

    Update CtoFTester.java

---
+++ CtoFTester.java
@@ -0,0 +1,14 @@
+   class Main {
+
+  public static int celsius(int num) {
+    return (f - 32) * (5/9);
+  }
+
+  public static void main(String[] args) {
+    int result;
+
+    result = celsius(32);
+
+    System.out.println("Fahrenheit of 32 degrees is (in celsius): " + result);
+  }
+}
